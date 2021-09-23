c6164e9863d495a277311dc66f06ed1045a89111
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-09-23 17:31:25 -0400

    feat: add while loop to repeat chorus line

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's dance!";
+let repeat = 0; //start
+while (repeat < 10) {  //stop
+  if (repeat === 5) { // with if...statement
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++; //step
+}
+console.log("Until the sun comes up!");
--- conditionals.js
+++ conditionals.js
@@ -41,5 +41,18 @@ if (!raining) {
 }
 
 
+const whichSchool = function (age){
+  let school;
+  if(age < 13){
+    school = 'Elementary School';
+  } else if (age >= 13 && age <= 18){
+    school = 'Secondary School';
+  } else {
+    school = 'Lighthouse Labs';
+  }
+  return school;
+}
+
+
 
 
