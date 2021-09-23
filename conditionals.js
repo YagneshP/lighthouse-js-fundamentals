44fa52a109657ce99544148319301fdb1b158bc6
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-09-23 07:03:40 -0400

    feat:Add more code exampler

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");
 
-
+//temprature
 const temperature = 12;
 
 if (temperature < 0) {
@@ -23,3 +23,23 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//logical operator
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+};
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+
+
